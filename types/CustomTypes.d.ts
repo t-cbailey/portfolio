020e1b119230d2816430dcd5802d483972b4c20d
t-cbailey <t.cbaileymusic@gmail.com>
2023-07-26 16:55:38 +0100

    installed and added firebase

--- types/CustomTypes.d.ts
+++ types/CustomTypes.d.ts
@@ -17,3 +17,12 @@ export interface Msg {
   subject: string;
   messageBody: string;
 }
+
+export interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
