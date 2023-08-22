a7d08a45cecef99ac0f756919cb2c99d33b3da69
t-cbailey <t.cbaileymusic@gmail.com>
2023-08-22 10:24:50 +0100

    added social links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,28 @@
 import "../styling/header.css";
-
+import linkedInLogo from "../assets/linkedin-logo.png";
+import gitHubLogo from "../assets/github-mark.png";
 function Header() {
   return (
     <>
-      <header>Tim Bailey Software Developer</header>
+      <header>
+        Tim Bailey Software Developer
+        <a href="https://www.linkedin.com/in/timbaileydev/" target="blank">
+          <img
+            src={linkedInLogo}
+            alt="linkedInLogo"
+            className="socialsIcon"
+            id="linkedIn"
+          />
+        </a>
+        <a href="https://github.com/t-cbailey" target="blank">
+          <img
+            src={gitHubLogo}
+            alt="gitHubLogo"
+            className="socialsIcon"
+            id="gitHub"
+          />
+        </a>
+      </header>
     </>
   );
 }
