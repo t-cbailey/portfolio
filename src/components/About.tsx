853128ae303ea070b1928da40519e12c81509ce2
t-cbailey <t.cbaileymusic@gmail.com>
2023-09-06 12:50:32 +0100

    added fade to scroll notifier

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,6 +9,7 @@ function About({ setCurrentPage }: AboutProps) {
   const [timer, setTimer] = React.useState(false);
   const [scrollDirection, setScrollDirection] = React.useState("scrollDown");
   const [scrollPostition, setScrollPosition] = React.useState(0);
+  const [renderCount, setRenderCount] = React.useState(0);
 
   React.useEffect(() => {
     setCurrentPage("About");
@@ -39,9 +40,13 @@ function About({ setCurrentPage }: AboutProps) {
   }
 
   React.useEffect(() => {
-    setTimeout(() => {
-      setTimer(true);
-    }, 2000);
+    setTimeout(
+      () => {
+        setTimer(true);
+      },
+      renderCount === 0 ? 2000 : 10000
+    );
+    setRenderCount(renderCount + 1);
   }, [timer]);
 
   React.useEffect(() => {
@@ -93,7 +98,14 @@ function About({ setCurrentPage }: AboutProps) {
               I'm a junior full stack software developer.
             </p>
           </section>
-          {timer && <div className="icon-scroll"></div>}
+
+          <>
+            <div
+              className={`scrollerContainer ${timer && "background-fade-in"}`}
+            ></div>
+            <div className={`icon-scroll ${timer && "icon-fade-in"}`}></div>
+          </>
+
           <section className="aboutText">
             <p className="aboutBody" id="aboutB">
               Having recently completed a software development bootcamp at{" "}
