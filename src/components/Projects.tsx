46701c2bb56eaa06d681ee088ab49b2ce28b4351
t-cbailey <t.cbaileymusic@gmail.com>
2023-09-26 16:00:01 +0100

    replaced css carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { getFile, getProjects } from "../../Utils/utils";
 import { ProjectProps, ProjectRes } from "../../types/CustomTypes";
 import { Link } from "react-router-dom";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function Projects({ setCurrentPage }: ProjectProps) {
   const [projects, setProjects] = React.useState<ProjectRes[]>([]);
@@ -60,21 +62,75 @@ function Projects({ setCurrentPage }: ProjectProps) {
     return (
       <>
         <h3 id="projectTitle">select a project...</h3>
-        <ul className="projectScroller">
-          {projects.map((project: ProjectRes) => {
-            return (
-              <li key={project.id}>
-                <Link key={project.id} to={project.id}>
-                  <video autoPlay loop muted playsInline key={project.id}>
-                    <source src={project.imgURLwebm} type="video/webm" />
-                    <source src={project.imgURLmp4} type="video/mp4" />
-                  </video>
-                  <h3 className="projectName"> {project.name}</h3>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        <div id="carouselContainer">
+          <Carousel
+            additionalTransfrom={0}
+            arrows
+            autoPlaySpeed={3000}
+            centerMode={true}
+            className=""
+            containerClass="container"
+            dotListClass=""
+            draggable
+            focusOnSelect={false}
+            infinite={false}
+            itemClass=""
+            keyBoardControl
+            minimumTouchDrag={80}
+            pauseOnHover
+            renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+              desktop: {
+                breakpoint: {
+                  max: 3000,
+                  min: 1024,
+                },
+                items: 3,
+                partialVisibilityGutter: 10,
+              },
+              mobile: {
+                breakpoint: {
+                  max: 464,
+                  min: 0,
+                },
+                items: 1,
+                partialVisibilityGutter: 30,
+              },
+              tablet: {
+                breakpoint: {
+                  max: 1024,
+                  min: 464,
+                },
+                items: 2,
+                partialVisibilityGutter: 30,
+              },
+            }}
+            rewind={false}
+            rewindWithAnimation={false}
+            rtl={false}
+            shouldResetAutoplay
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={1}
+            swipeable
+          >
+            {projects.map((project: ProjectRes) => {
+              return (
+                <div className="projectContainer" key={project.id}>
+                  <Link key={project.id} to={project.id}>
+                    <video autoPlay loop muted playsInline key={project.id}>
+                      <source src={project.imgURLwebm} type="video/webm" />
+                      <source src={project.imgURLmp4} type="video/mp4" />
+                    </video>
+                    <h3 className="projectName"> {project.name}</h3>
+                  </Link>
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
       </>
     );
 }
