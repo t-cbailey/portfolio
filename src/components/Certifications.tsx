1a890408bbd69a85ef5f52b18a25877d7d873c0e
t-cbailey <t.cbaileymusic@gmail.com>
2024-01-09 17:09:52 +0000

    added modal to certs page

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -6,6 +6,8 @@ import { CertificationsProps } from "../../types/CustomTypes";
 function Certifications({ setCurrentPage }: CertificationsProps) {
   const [certs, setCerts] = React.useState<string[]>([]);
   const [loading, setLoading] = React.useState(false);
+  const [currCert, setCurrCert] = React.useState<string>("");
+  const [overlayHidden, setOverlayHidden] = React.useState<boolean>(true);
 
   React.useEffect(() => {
     setCurrentPage("Certifications");
@@ -17,7 +19,12 @@ function Certifications({ setCurrentPage }: CertificationsProps) {
       fileNames &&
         fileNames.forEach((imgUrl) => {
           getFile(imgUrl).then((URL) => {
-            URL && setCerts((curr) => [...curr, URL]);
+            URL &&
+              setCerts((curr) => {
+                if (!curr.includes(URL)) {
+                  return [...curr, URL];
+                } else return curr;
+              });
             setLoading(false);
           });
         });
@@ -27,14 +34,61 @@ function Certifications({ setCurrentPage }: CertificationsProps) {
   if (loading) {
     return <h3 className="loading">Loading...</h3>;
   }
+
+  const screenWidth = window.innerWidth;
+
+  console.log(screenWidth);
+
   return (
     <>
+      {!overlayHidden && screenWidth > 1200 && (
+        <section
+          style={{
+            position: "fixed",
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div id="overlayBackground"></div>
+          <img id="certificateExpanded" src={currCert} alt={currCert} />
+          <div
+            onClick={() => {
+              setOverlayHidden(true);
+              setCurrCert("");
+            }}
+            style={{
+              position: "absolute",
+              top: 15,
+              right: 35,
+              color: "white",
+              fontSize: 40,
+              fontWeight: "bold",
+              cursor: "pointer",
+              zIndex: "99",
+            }}
+          >
+            &times;
+          </div>
+        </section>
+      )}
       <ul id="certlist">
         {certs.map((cert, i) => {
           return (
-            <li key={cert + i} id="certlistitem">
-              <img src={cert} alt={cert} className="certificate" />
-            </li>
+            <button
+              key={cert + i}
+              onClick={() => {
+                setCurrCert(cert);
+                setOverlayHidden(false);
+              }}
+              id="certlistitem"
+            >
+              <li>
+                <img src={cert} alt={cert} className="certificate" />
+              </li>
+            </button>
           );
         })}
       </ul>
